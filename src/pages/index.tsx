e8e3bc1e282c18ff84e48ef87d7c7eb5f7642b53
RoyAppeldoorn <roy.appeldoorn@gmail.com>
2022-11-15 13:26:21 +0100

    Refactored ui-components for increased Separation of Concerns

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,12 @@
 import type { NextPage } from "next";
-import Head from "next/head";
+import Image from "next/image";
 import { useCallback, useState } from "react";
-import { useThemeContext } from "../contexts/ThemeContext";
 import { useSpotifyWebSDK } from "../hooks/useSpotifyWebSDK";
 import styles from "../styles/Home.module.css";
-import { ThemeToggle } from "../ui-components";
-import { Navbar } from "../ui-components";
 
 const Home: NextPage = () => {
   const [paused, setPaused] = useState<boolean>();
   const [currentTrack, setCurrentTrack] = useState<Spotify.Track>();
-  const { isDarkMode, toggle } = useThemeContext();
 
   const onPlayerStateChanged = useCallback((state: Spotify.PlaybackState) => {
     if (!state) {
@@ -30,51 +26,45 @@ const Home: NextPage = () => {
   });
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Spotify audio game</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div>
+      {player && isReady && (
+        <>
+          <button
+            onClick={async () => {
+              await player?.previousTrack();
+            }}
+          >
+            &lt;&lt;
+          </button>
+          <button
+            onClick={async () => {
+              await player?.togglePlay();
+            }}
+          >
+            {paused ? "PLAY" : "PAUSE"}
+          </button>
+          <button
+            onClick={async () => {
+              await player?.nextTrack();
+            }}
+          >
+            &gt;&gt;
+          </button>
 
-      <Navbar />
-
-      <div className={styles.main}>
-        <ThemeToggle isDarkMode={isDarkMode} toggle={toggle} />
-
-        {player && isReady && (
-          <>
-            <button
-              onClick={async () => {
-                await player?.previousTrack();
-              }}
-            >
-              &lt;&lt;
-            </button>
-            <button
-              onClick={async () => {
-                await player?.togglePlay();
-              }}
-            >
-              {paused ? "PLAY" : "PAUSE"}
-            </button>
-            <button
-              onClick={async () => {
-                await player?.nextTrack();
-              }}
-            >
-              &gt;&gt;
-            </button>
-
-            {currentTrack && (
-              <>
-                <img src={currentTrack.album.images[0].url} alt="" />
-                <div>{currentTrack.name}</div>
-                <div>{currentTrack.artists[0].name}</div>
-              </>
-            )}
-          </>
-        )}
-      </div>
+          {currentTrack && (
+            <>
+              <Image
+                src={currentTrack.album.images[0].url}
+                alt=""
+                width={250}
+                height={250}
+              />
+              <div>{currentTrack.name}</div>
+              <div>{currentTrack.artists[0].name}</div>
+            </>
+          )}
+        </>
+      )}
     </div>
   );
 };
--- src/ui-components/Navbar/Navbar.tsx
+++ src/ui-components/Navbar/Navbar.tsx
@@ -1,24 +1,12 @@
-import { signIn, signOut, useSession } from "next-auth/react";
+import { PropsWithChildren } from "react";
 import styles from "./Navbar.module.css";
 
-export const Navbar = () => {
-  const { data: session } = useSession();
-
+export const Navbar: React.FC<PropsWithChildren<{}>> = ({ children }) => {
   return (
     <nav className={styles.menu}>
       <div className={styles.menuContent}>
         <span>Audiosurf</span>
-        {session ? (
-          <span>
-            Signed in as {session.user?.name}
-            <button onClick={() => signOut()}>Sign out</button>
-          </span>
-        ) : (
-          <span>
-            Not signed in
-            <button onClick={() => signIn()}>Sign in</button>
-          </span>
-        )}
+        <div className={styles.menuItems}>{children}</div>
       </div>
     </nav>
   );
